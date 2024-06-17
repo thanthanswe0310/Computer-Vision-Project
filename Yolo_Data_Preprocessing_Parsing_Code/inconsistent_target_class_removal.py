671cf676307f9c4d2ef5aefb256a0498952c7e5f
TTS <148617564+thanthanswe0310@users.noreply.github.com>
2024-06-17 23:59:21 +0900

    Update inconsistent_target_class_removal.py
    updated file

--- Yolo_Data_Preprocessing_Parsing_Code/inconsistent_target_class_removal.py
+++ Yolo_Data_Preprocessing_Parsing_Code/inconsistent_target_class_removal.py
@@ -1,41 +1,41 @@
 import os
 
 def remove_classes_and_coordinates(folder_path, target_classes):
-    # 폴더 내의 모든 txt 파일 가져오기
+   # Import all txt files within a folder
     txt_files = [f for f in os.listdir(folder_path) if f.endswith(".txt")]
 
-    # 각 파일에 대해 작업 수행
+   # Perform actions for each file
     for file_name in txt_files:
         file_path = os.path.join(folder_path, file_name)
 
-        # 파일 읽기
+       # Read the file
         with open(file_path, 'r') as file:
             lines = file.readlines()
 
-        # 새로운 내용을 저장할 리스트
+       # List to save new content
         new_lines = []
 
-        # 각 줄에 대해 검사하여 특정 클래스 번호와 좌표값 제거
+       # Inspect each line to remove specific class numbers and coordinate values
         for line in lines:
             parts = line.split(' ')
             current_class = parts[0]
 
-            # 제거할 클래스 번호와 일치하지 않으면 추가
+           # Add if you do not match the class number you want to remove
             if current_class not in target_classes:
                 new_lines.append(line)
 
-        # 파일에 새로운 내용 쓰기
+       # Write new content to a file
         with open(file_path, 'w') as file:
             file.writelines(new_lines)
 
 if __name__ == "__main__":
-    # 작업할 폴더 경로
-    folder_path = "F:\yolov7-main\coco128/valid\labels/"
+    # Folder path to work with
+    folder_path = "../../labels/"
 
-    # 제거할 클래스 번호들
+   # Class Numbers to Remove
     target_classes = ["2","4","6"]
 
-    # 함수 호출
+   # Function call
     remove_classes_and_coordinates(folder_path, target_classes)
 
-    print(f"클래스 {', '.join(target_classes)}와 해당 좌표값을 제거하는 작업이 완료되었습니다.")
+   print(f"Class {', '.join(target_classes)} and its coordinate values have been removed.")
