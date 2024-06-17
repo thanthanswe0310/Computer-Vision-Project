de57ccc22d7b8a35389dbed278475317ca601971
TTS <148617564+thanthanswe0310@users.noreply.github.com>
2024-06-18 00:19:33 +0900

    Update passing9.py
    modified

--- Yolo_Data_Preprocessing_Parsing_Code/passing9.py
+++ Yolo_Data_Preprocessing_Parsing_Code/passing9.py
@@ -1,39 +1,39 @@
 import os
 
-# 처리할 폴더 경로
-input_folder_path = "E:\yolov7-main\coco128/train\labels/"
-output_folder_path = "E:\yolov7-main\coco128/train01/"
+# Folder path to process
+input_folder_path = "..\..\coco128/train\labels/"
+output_folder_path = "..\..\coco128/train01/"
 target_class = "9"
-# 출력 폴더가 없으면 생성
+# Create if you don't have an output folder
 if not os.path.exists(output_folder_path):
     os.makedirs(output_folder_path)
 
-# 입력 폴더의 모든 .txt 파일을 가져와서 처리
+# Import and process all .txt files in the input folder
 for filename in os.listdir(input_folder_path):
     if filename.endswith(".txt"):
         input_file_path = os.path.join(input_folder_path, filename)
         output_file_path = os.path.join(output_folder_path, filename)
 
-        # 파일을 읽어오고 새로운 내용을 저장할 리스트 생성
+       # Create a list to read files and save new content
         new_lines = []
 
-        # 파일 읽기
+      # Read the file
         with open(input_file_path, "r") as file:
             lines = file.readlines()
 
-        # 클래스 정보와 좌표 값을 추출하고 필요한 경우에만 저장
+      # extract class information and coordinate values only if necessary
         for line in lines:
             parts = line.strip().split()
             if len(parts) >= 5 and parts[0] != target_class:
                 new_lines.append(line)
             elif len(parts) < 5:
-                # 클래스 정보가 없는 빈 줄도 유지
+               # Keep empty lines without class information
                 new_lines.append(line)
 
-        # 새로운 내용을 파일로 저장
+      # Save a new content to file
         with open(output_file_path, "w") as file:
             file.writelines(new_lines)
 
         print("Done:", output_file_path)
 
-print("모든 파일 처리 완료")
+print ("All files processed")
